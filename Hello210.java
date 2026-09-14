0b2b22ac7ddde07ad12f2a0aa3213fc622491100
leyonce <lk15palm@siena.edu>
2026-09-14 09:57:14 -0400

    Added name and extra printout.

--- Hello210.java
+++ Hello210.java
@@ -2,12 +2,15 @@
 /**
  * CSIS 210 Setup Lab "Hello, World" program
  *
- * @author Prof. White
+ * @author leyonce palmer
  */
 public class Hello210 {
     
     public static void main(String args[]) {
         
         System.out.println("Hello CSIS 210 World!");
+        System.out.println("Delmar, NY");
+        System.out.println("Cookoo's Nest");
+        System.out.println("Chicken and Waffles");
     }
 }
